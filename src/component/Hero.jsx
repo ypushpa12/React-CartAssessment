1f4383e4332334f6d2f6eb6bb2c202793a9824ca
ypushpa12 <pushpalatha.yarra@gspann.com>
2022-07-21 23:11:25 +0530

    changes

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -33,7 +33,7 @@ function Hero() {
         <div class="aem-Grid aem-Grid--12">
           <div class="aem-GridColumn aem-GridColumn--default--8 aem-GridColumn--phone--12">
             <div class="card-img">
-              <img src="https://www.zdnet.com/a/img/resize/aae4500dcc80cf4f9c59bd066c4db2fa96c1b02c/2021/04/16/8006dbda-20d3-40fb-9cf4-bc6c30ef0cce/shutterstock-526104310.jpg?width=770&height=578&fit=crop&auto=webp" class="card-img-top" alt="" />
+              <img src="https://img.freepik.com/free-photo/shopping-bag-cart_23-2148879372.jpg?w=2000" class="card-img-top" alt="" />
             </div>
           </div>
 
